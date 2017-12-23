476ee5c91b46b6f999d13f14406f7e77daccd693
SmileCJX <1092925421@qq.com>
2017-12-23 20:02:34 +0800

    输出属性

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OrderComponent } from './order/order.component';
+import {FormsModule} from "@angular/forms";
+import { PriceQuoteComponent } from './price-quote/price-quote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderComponent,
+    PriceQuoteComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
